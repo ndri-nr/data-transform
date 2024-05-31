c2de41693a25c51e76f2e1182c45612a84290139
Andri Nur Rochman <ceoandri@gmail.com>
2024-05-31 10:38:15 +0700

    remove unused import

--- src/main/java/com/kpei/mkbd/datatransform/App.java
+++ src/main/java/com/kpei/mkbd/datatransform/App.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
